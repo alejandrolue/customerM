22b85370966167cbdb9d0ca508e784ee859682e5
Alejandro Lüthi <alejandro.luthi@gmail.com>
2024-06-25 02:57:54 +0200

    installed react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import PlayGround from './pages/playGround/playGround';
+import HomePage from './pages/homepage/homePage';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+            <Routes>
+                <Route exact path="/" element={<HomePage />}/>
+                <Route path="/playground" element={<PlayGround/>}/>
+            </Routes>
+        </Router>
+        
     </div>
   );
 }
