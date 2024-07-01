5a2c056c335a52a1338ac0ad342601376a187db4
Alejandro Lüthi <alejandro.luthi@gmail.com>
2024-07-01 23:14:36 +0200

    nothing much changed

--- src/pages/homepage/homePage.js
+++ src/pages/homepage/homePage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Login from "../../components/login/login";
 
 export default function HomePage() {
     return (
         <div className="homePage-container">
-
+            <Login/>
         </div>
     )
 }
---
+++ src/pages/loginPage/loginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loginPage.css"
+
+export default function LoginPage() {
+    return (
+        <div className="loginPage-container">
+            <div className="loginPage-card">
+                <div className="loginPage-content">
+                    <h2>Body</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
